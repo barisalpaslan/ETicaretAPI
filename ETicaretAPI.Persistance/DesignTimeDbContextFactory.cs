037f0e98a2d4751f4e96ea40feb185295d127972
barisalpaslan <brsaslan333@gmail.com>
2024-08-28 11:08:01 +0300

    Add project files.

---
+++ ETicaretAPI.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using ETicaretAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ETicaretAPI.Persistance
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            //ConfigurationManager configurationManager = new();
+            //configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+            //configurationManager.AddJsonFile("appsettings.json");
+
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(DbConfigurations.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
